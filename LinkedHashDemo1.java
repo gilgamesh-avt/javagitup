116a051b58cc06de9dae77c3cb432e19701a1d11
Ruksar02 <sayyadr026@gmail.com>
2022-11-18 18:48:57 +0530

    Second commit

---
+++ LinkedHashDemo1.java
@@ -0,0 +1,34 @@
+package DemoSet;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+public class LinkedHashDemo1 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LinkedHashSet hs= new LinkedHashSet();
+		
+		hs.add("Akash");
+		hs.add("Prajakta");
+		hs.add("Tiwari");
+		hs.add("sayali");
+		hs.add("Sudhir");
+		hs.add("Ruksar");
+		
+	//	System.out.println(hs);
+		
+	/*	for (Object o : hs) {
+			
+			System.out.println(o);
+		}*/
+		
+		Iterator i=hs.iterator();
+		while(i.hasNext())
+		{
+			System.out.println(i.next());
+		}
+	}
+
+}
